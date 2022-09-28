d35591973aca9dd9b98c8e278e6642973515d63f
Yesu padam <yesupadam2001@gmail.com>
2022-09-28 20:47:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {Container,Box,Grid,Button} from '@mui/material';
+import Scarecrow from './Scarecrow.png';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 function App() {
+  const matches = useMediaQuery('(min-width:400px)');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="xl">
+        <Box className="BOX" sx={{ height: '100vh', }}>
+          <p>404 NOT FOUND</p>
+          <Grid container spacing={2} sx={{marginTop:'100px',}}>
+            <Grid item xs={6}>
+              <img src={Scarecrow} alt="scarecrow"/>
+            </Grid>
+            <Grid item xs={6} sx={{display:'flex',flexDirection:'column'}}>
+              <b style={{fontSize:'70px',color: '#333333'}}>I have bad news for you</b>
+              <span>The page you are looking for might be removed or is temporarily unavailable</span>
+             <Button variant="contained" sx={{width:"200px",backgroundColor: '#333333',fontSize: '12px', borderRadius: '0px'}} size="large">BACK TO HOMEPAGE</Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
     </div>
   );
 }
